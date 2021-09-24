e1aac5df561d7e2f26d8b0fbe147edab2db7c570
Saad Sagheer <i190496@nu.edu.pk>
2021-09-24 09:26:37 +0500

    Initial Commit of Sara Pubg Wali

---
+++ jdakfal/src/main/java/jdakfal/BCKKBMC/SaraPubgWali.java
@@ -0,0 +1,12 @@
+package jdakfal.BCKKBMC;
+
+public class SaraPubgWali {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mein KKB MC b hu");
+		
+		System.out.println("Hello");
+	}
+
+}
